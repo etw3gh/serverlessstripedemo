938b93011d7f7e0ba3bad84a9a4fab2cb51002a6
Eli Tabello <etabw3@gmail.com>
2017-06-06 17:06:00 -0400

    'try catch in ses send'

--- sendemail.js
+++ sendemail.js
@@ -3,60 +3,71 @@ const AWS = require('aws-sdk')
 
 module.exports.sendemailHandler = (event, context, callback) => {
 
-  let ses = new AWS.SES({region: process.env.REGION})
+  try {
+    let ses = new AWS.SES({region: process.env.REGION})
 
-  const bodyData = event.body.bodyData
-  const bodyCharset = 'UTF-8'
-  const subjectdata = event.body.subjectdata
-  const subjectCharset = bodyCharset
-  const sourceEmail = process.env.SES_SENDER
+    const bodyData = event.body.bodyData
+    const bodyCharset = 'UTF-8'
+    const subjectdata = event.body.subjectdata
+    const subjectCharset = bodyCharset
+    const sourceEmail = process.env.SES_SENDER
 
-  let emailParams = {
-    Destination: {
-      BccAddresses: [process.env.SES_SENDER],
-      CcAddresses: [],
-      ToAddresses: [process.env.SES_SENDER]
-    },
-    Message: {
-      Body: {
-        Text: {
-          Data: bodyData,
-          Charset: bodyCharset
+    let emailParams = {
+      Destination: {
+        BccAddresses: [process.env.SES_SENDER],
+        CcAddresses: [],
+        ToAddresses: [process.env.SES_SENDER]
+      },
+      Message: {
+        Body: {
+          Text: {
+            Data: bodyData,
+            Charset: bodyCharset
+          }
+        },
+        Subject: {
+          Data: subjectdata,
+          Charset: subjectCharset
         }
       },
-      Subject: {
-        Data: subjectdata,
-        Charset: subjectCharset
-      }
-    },
-    Source: sourceEmail,
-    ReplyToAddresses: process.env.SES_SENDER
-  }
+      Source: sourceEmail,
+      ReplyToAddresses: process.env.SES_SENDER
+    }
 
-  // obtain a promise
-  const sesPromise = ses.sendEmail(emailParams)
+    // obtain a promise
+    const sesPromise = ses.sendEmail(emailParams)
 
 
-  // handle promise
-  sesPromise.then( sesResponse => {
+    // handle promise
+    sesPromise.then( sesResponse => {
+        const response = {
+          statusCode: 200,
+          body: JSON.stringify({
+            message: 'SUCCESS',
+            sesResponse: sesResponse
+          })
+        }
+        callback(null, response)
+    }).catch( e => {
+        const response = {
+          statusCode: 500,
+          body: JSON.stringify({
+            message: 'SES SENDING ERROR',
+            input: event,
+            error: e
+          })
+        }
+        callback(null, response)
+    })
+  } catch(e) {
       const response = {
-        statusCode: 200,
+        statusCode: 502,
         body: JSON.stringify({
-          message: 'SUCCESS',
-          sesResponse: sesResponse
-        })
-      }
-      callback(null, response)
-  }).catch( e => {
-      const response = {
-        statusCode: 500,
-        body: JSON.stringify({
-          message: 'SES ERROR',
+          message: 'SES SETUP ERROR',
           input: event,
           error: e
         })
       }
       callback(null, response)
-  })
-
+  }
 }
